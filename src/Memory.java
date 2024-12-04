4da1a54694c3652ca3df2e12ecd98600a3ab2ca3
killarney99 <killarney@web.de>
2024-12-04 22:36:11 +0100

    First commit: Create Memory Field

---
+++ src/Memory.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Memory {
+
+    public static void main(String[] args) {
+        // Erstellen des Fensters
+        JFrame frame = new JFrame("Memory Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        
+        // Fenster sichtbar machen
+        frame.setVisible(true);
+    }
+}
