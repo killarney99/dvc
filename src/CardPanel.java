02263743990d85ad3024557cfffe1d41621383df
killarney99 <killarney@web.de>
2024-12-05 22:17:03 +0100

    show binary numbers on cards

--- src/CardPanel.java
+++ src/CardPanel.java
@@ -13,11 +13,13 @@ class CardPanel extends JPanel {
         super.paintComponent(g);
         
         // Gitter zeichnen (4x4)
-        int cardNumber = 1;
+        String[] binaryNumbers = {"0000","0001","0010","0011","0100","0101","0110","0111",
+                                  "1000","1001","1010","1011","1100","1101","1110","1111"};
+        int cardNumber = 0;
         for (int row = 0; row < ROWS; row++) {
             for (int col = 0; col < COLS; col++) {
                 // Berechnen der Position des Tiles
-            	Card c = new Card(Integer.toString(cardNumber));
+            	Card c = new Card(binaryNumbers[cardNumber]);
                 int x = col * c.getCardSize();
                 int y = row * c.getCardSize();
 
@@ -26,7 +28,7 @@ class CardPanel extends JPanel {
                 g.fillRect(x, y, c.getCardSize(), c.getCardSize());
                 g.setColor(c.getBorderColor()); // Farbe des Rahmens
                 g.drawRect(x, y, c.getCardSize(), c.getCardSize());
-                g.drawString(c.getContent(), x + c.getCardSize()/2, y + c.getCardSize()/2);
+                g.drawString(c.getContent(), x + c.getCardSize()/3, y + c.getCardSize()/2);
                 cardNumber++;
             }
         }
